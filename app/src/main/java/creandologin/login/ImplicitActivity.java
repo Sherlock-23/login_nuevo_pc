272535d917aef94be9765c6f443414d24a55f954
Sherlock-23 <gonzalo.arck@gmail.com>
2016-10-24 22:52:20 -0300

    En esta version me di cuenta que no puedo hacer que el usuario sea escrito con letras,numeros y caracteres como -_, es por eso que se procedera con otro tipo de validacion.

--- app/src/main/java/creandologin/login/ImplicitActivity.java
+++ app/src/main/java/creandologin/login/ImplicitActivity.java
@@ -132,9 +132,10 @@ public class ImplicitActivity extends Activity implements Validator.ValidationLi
 
                 // check if any of the fields are vaccant
                 if (userName.equals("") || password.equals("") || confirmPassword.equals("")) {
-                    //Toast.makeText(getApplicationContext(), "Field Vaccant", Toast.LENGTH_LONG).show();
+                    //Toast.makeText(getApplicationContext(), "Al usuario le faltan expresiones regulares", Toast.LENGTH_LONG).show();
                     return;
                 }
+
                 // check if both password matches
                 if (!password.equals(confirmPassword)) {
                     Toast.makeText(getApplicationContext(), "Password does not match", Toast.LENGTH_LONG).show();
