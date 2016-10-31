b8230576ef01c59366c4b4dd695d5f99ff2575f4
Sherlock-23 <gonzalo.arck@gmail.com>
2016-10-31 17:20:25 -0300

    Agregando las imagenes del menu principal del aplicativo.

--- app/src/main/java/creandologin/login/menu_principal.java
+++ app/src/main/java/creandologin/login/menu_principal.java
@@ -1,11 +1,18 @@
 package creandologin.login;
 
+import android.content.Intent;
 import android.graphics.Color;
+import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class menu_principal extends AppCompatActivity {
+
+    ImageView pulsardescanso;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,5 +22,15 @@ public class menu_principal extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         toolbar.setTitle("Menu principal");
         toolbar.setTitleTextColor(Color.WHITE);
+
+
+        pulsardescanso = (ImageView)findViewById(R.id.imageView5);
+        pulsardescanso.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent descanso = new Intent(menu_principal.this, Principal.class);
+                startActivity(descanso);
+            }
+        });
     }
 }
